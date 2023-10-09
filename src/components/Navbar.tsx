365468a85b104a829da374534c84de69f028b11b
neoncodes <41237443+charleywolf@users.noreply.github.com>
2023-10-09 01:03:57 -0400

    updated navbar, added link to source code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,18 @@
+import { ChevronDoubleRightIcon } from "@heroicons/react/24/outline";
+
 export default function Navbar() {
   return (
-    <nav className="px-12 py-8 text-center bg-stone-800">
+    <nav className="bg-gray-800 py-7 px-8 text-center">
       <h1 className="text-4xl font-bold text-white">
-        Loan Calculator by Charley Wolf
+        Charley Wolf&apos;s Loan Calculator
       </h1>
+      <a
+        href="https://github.com/charleywolf/loan-calculator"
+        className="text-blue-400 hover:text-blue-600 hover:underline mt-2 flex justify-center items-center"
+      >
+        View the Source Code on GitHub
+        <ChevronDoubleRightIcon className="h-3 w-3" />
+      </a>
     </nav>
   );
 }
